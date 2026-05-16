b75aeee19dd34ac57ebc98aae7e4030587e11f5d
ysy <yemoletrader@gmail.com>
2026-05-16 01:09:46 +0000

    update finale

--- Tracao-backend/build.sh
+++ Tracao-backend/build.sh
@@ -8,14 +8,13 @@ pip install -r requirements.txt
 echo "🎨 Collecte des fichiers statiques..."
 python tracao/manage.py collectstatic --no-input
 
+echo "🚨 ATTENTION: Réinitialisation complète de la BD (À SUPPRIMER APRÈS CE DÉPLOIEMENT) !"
+python tracao/reset_db.py
+
 echo "🗄️ Application des migrations (création des tables)..."
 python tracao/manage.py migrate
 
 echo "👑 Création ou mise à jour du Super Admin..."
 python tracao/manage.py set_superuser
-
-echo " Là c'est pour la BD oooo"
-python python tracao/reset_db.py
-
 
 echo "✅ Build terminé avec succès !"
