f16a5d781c4e8a917bbac380f1dde86a0e1d73c1
ysy <yemoletrader@gmail.com>
2026-05-16 03:00:44 +0000

    update

--- Tracao-backend/tracao/stock/api.py
+++ Tracao-backend/tracao/stock/api.py
@@ -1,101 +1,126 @@
-from ninja_extra import api_controller,route
-from ninja_extra.permissions import IsAuthenticated,AllowAny
-from stock.schemas import StockProducerSchema,StockProducerCreate, FarmCreateSchema, FarmResponseSchema, StockOriginSchema, StockOriginCreate, StockTransporterSchema, StockTransporterCreate, StockDestinationSchema, StockDestinationCreate
-from stock.models import StockProducer, Farm, StockOrigin, StockTransporter, StockDestination
-from user.models import TracaoUser, Notification
+from ninja_extra import api_controller, route
+from ninja.responses import Response
+from typing import List, Optional
 from django.shortcuts import get_object_or_404
+from ninja_jwt.authentication import JWTAuth
+from stock.models import Parcel, Batch, Harvest, BatchTransfer
+from stock.schemas import (
+    ParcelCreateSchema, ParcelResponseSchema, ParcelUpdateSchema,
+    BatchCreateSchema, BatchResponseSchema, BatchUpdateSchema,
+    HarvestCreateSchema, HarvestResponseSchema,
+    BatchTransferCreateSchema, BatchTransferResponseSchema
+)
+from user.models import TracaoUser
+from ninja.errors import HttpError
 
 
-@api_controller('/stock',auth=None)
+
+@api_controller('/stock', auth=JWTAuth())
 class StockController:
-    @route.post('/stock_producer',response=StockProducerSchema)
-    def create_stock_producer(self,stock_producer:StockProducerCreate):
-        data = stock_producer.model_dump()
-        farm_id = data.pop('farm_id', None)
-        producer_id = data.pop('producer')
-        coop_id = data.pop('cooperative')
+
+    # PARCELS (EUDR Compliance)
+
+    @route.get('/parcels', response=List[ParcelResponseSchema])
+    def list_parcels(self, request):
+        return Parcel.objects.filter(farmer=request.user)
+
+    @route.post('/parcels', response=ParcelResponseSchema)
+    def create_parcel(self, request, data: ParcelCreateSchema):
+        parcel = Parcel.objects.create(**data.dict())
+        return parcel
+
+    @route.get('/parcels/{parcel_id}', response=ParcelResponseSchema)
+    def get_parcel(self, request, parcel_id: str):
+        return get_object_or_404(Parcel, id=parcel_id, farmer=request.user)
+
+    @route.patch('/parcels/{parcel_id}', response=ParcelResponseSchema)
+    def update_parcel(self, request, parcel_id: str, data: ParcelUpdateSchema):
+        parcel = get_object_or_404(Parcel, id=parcel_id)
         
-        stock = StockProducer.objects.create(
-            producer_id=producer_id,
-            cooperative_id=coop_id,
-            farm_id=farm_id,
-            **data
-        )
+        # Seul le propriétaire ou un gestionnaire de magasin peut modifier
+        if parcel.farmer != request.user and not request.user.is_store:
+            raise HttpError(403, "Non autorisé")
 
-        # Créer une notification pour le magasinier/coopérative
-        Notification.objects.create(
-            user_id=coop_id,
-            type='LOT_CREATED',
-            message=f"Nouveau lot de {stock.weight}kg de {stock.product_type} reçu de {stock.producer.first_name} {stock.producer.last_name}.",
-            metadata={'lot_id': stock.id, 'producer_id': producer_id}
-        )
-
-        return stock
-
-    @route.get('/all_stock_producer',response=list[StockProducerSchema])
-    def get_all_stock_producer(self):
-        return StockProducer.objects.all()
-
-    @route.get('/producer_stocks/{producer_id}', response=list[StockProducerSchema])
-    def get_producer_stocks(self, producer_id: int):
-        return StockProducer.objects.filter(producer_id=producer_id)
-
-    @route.post('/stock_origin', response=StockOriginSchema)
-    def create_stock_origin(self, data: StockOriginCreate):
-        origin = StockOrigin.objects.create(**data.model_dump())
-        stock = origin.producer_stock
+        for attr, value in data.dict(exclude_unset=True).items():
+            setattr(parcel, attr, value)
         
-        # Créer une notification pour le producteur
-        Notification.objects.create(
-            user=stock.producer,
-            type='LOT_VALIDATED',
-            message=f"Votre lot de {stock.weight}kg de {stock.product_type} a été validé par le magasinier {origin.cooperative.org_name or origin.cooperative.email}.",
-            metadata={'lot_id': stock.id, 'batch_number': stock.batch_number}
-        )
+        # Si c'est un magasin qui valide, on enregistre qui a fait l'action
+        if request.user.is_store and data.status:
+            parcel.validated_by = request.user
+            
+        parcel.save()
+        return parcel
+
+
+    # BATCHES (Traceability Units)
+
+    @route.get('/batches', response=List[BatchResponseSchema])
+    def list_batches(self, request, store_id: Optional[int] = None, farmer_id: Optional[int] = None):
+        qs = Batch.objects.all()
+        if store_id:
+            qs = qs.filter(validated_by_id=store_id)
+        if farmer_id:
+            qs = qs.filter(farmer_id=farmer_id)
+        return qs
+
+    @route.post('/batches', response=BatchResponseSchema)
+    def create_batch(self, request, data: BatchCreateSchema):
+        # Générer un code unique s'il n'est pas fourni
+        if not data.unique_code:
+            import datetime
+            year = datetime.date.today().year
+            count = Batch.objects.count() + 1
+            data.unique_code = f"TRC-{year}-{count:04d}"
+            
+        batch = Batch.objects.create(**data.dict())
+        return batch
+
+    @route.get('/batches/farmer/{farmer_id}', response=List[BatchResponseSchema], auth=None)
+    def list_farmer_batches(self, request, farmer_id: int):
+        """Endpoint public pour récupérer les lots d'un agriculteur spécifique."""
+        return Batch.objects.filter(farmer_id=farmer_id).order_by('-created_at')
+
+    @route.get('/batches/{batch_id}', response=BatchResponseSchema)
+    def get_batch(self, request, batch_id: str):
+        return get_object_or_404(Batch, id=batch_id)
+
+    @route.patch('/batches/{batch_id}', response=BatchResponseSchema)
+    def update_batch(self, request, batch_id: str, data: BatchUpdateSchema):
+        batch = get_object_or_404(Batch, id=batch_id)
         
-        return origin
+        if batch.farmer != request.user and not request.user.is_store:
+            raise HttpError(403, "Non autorisé")
 
-    @route.get('/all_stock_origin', response=list[StockOriginSchema])
-    def get_all_stock_origin(self):
-        return StockOrigin.objects.all()
+        for attr, value in data.dict(exclude_unset=True).items():
+            setattr(batch, attr, value)
 
-    @route.get('/cooperative_stocks/{cooperative_id}', response=list[StockOriginSchema])
-    def get_cooperative_stocks(self, cooperative_id: int):
-        return StockOrigin.objects.filter(cooperative_id=cooperative_id)
+        if request.user.is_store and data.status:
+            batch.validated_by = request.user
 
-    @route.post('/stock_transporter', response=StockTransporterSchema)
-    def create_stock_transporter(self, data: StockTransporterCreate):
-        return StockTransporter.objects.create(**data.model_dump())
+        batch.save()
+        return batch
 
-    @route.get('/all_stock_transporter', response=list[StockTransporterSchema])
-    def get_all_stock_transporter(self):
-        return StockTransporter.objects.all()
 
-    @route.post('/stock_destination', response=StockDestinationSchema)
-    def create_stock_destination(self, data: StockDestinationCreate):
-        return StockDestination.objects.create(**data.model_dump())
+    # HARVESTS (Weight logs)
 
-    @route.get('/all_stock_destination', response=list[StockDestinationSchema])
-    def get_all_stock_destination(self):
-        return StockDestination.objects.all()
+    @route.post('/harvests', response=HarvestResponseSchema)
+    def create_harvest(self, request, data: HarvestCreateSchema):
+        harvest = Harvest.objects.create(**data.dict())
+        return harvest
 
-    @route.post('/farms', response=FarmResponseSchema)
-    def create_farm(self, farm_data: FarmCreateSchema):
-        data = farm_data.model_dump()
-        producer_id = data.pop('producer_id')
-        farm = Farm.objects.create(producer_id=producer_id, **data)
-        return farm
 
-    @route.get('/farms', response=list[FarmResponseSchema])
-    def get_all_farms(self):
-        return Farm.objects.all()
+    # TRANSFERS (Chain of Custody)
 
-    @route.post('/farms/{farm_id}/verify', response=FarmResponseSchema)
-    def verify_farm(self, farm_id: int, cooperative_id: int):
-        farm = get_object_or_404(Farm, id=farm_id)
-        farm.is_verified_by_coop = True
-        farm.verified_by_id = cooperative_id
-        farm.save()
-        return farm
-    
-    
+    @route.post('/transfers', response=BatchTransferResponseSchema)
+    def create_transfer(self, request, data: BatchTransferCreateSchema):
+        transfer = BatchTransfer.objects.create(**data.dict())
+        return transfer
+
+    @route.patch('/transfers/{transfer_id}/confirm', response=BatchTransferResponseSchema)
+    def confirm_transfer(self, request, transfer_id: str):
+        transfer = get_object_or_404(BatchTransfer, id=transfer_id, receiver=request.user)
+        transfer.status = 'confirmed'
+        import django.utils.timezone
+        transfer.confirmed_at = django.utils.timezone.now()
+        transfer.save()
+        return transfer
